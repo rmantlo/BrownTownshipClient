5920c98eb6168f7d7f1ef315873e0cf3e42760f9
Becky <you@example.com>
2019-09-16 17:02:09 -0400

    downloads updates

--- src/components/assistance/assistance.js
+++ src/components/assistance/assistance.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './assistance.css';
-import TA1Form from  '../../assets/files/Township Assistance Application I.pdf';
+import TA1Form from  '../../assets/files/TownshipAssistanceApplication.pdf';
 import Guidelines from '../../assets/files/2019FEDpovertyguidelines.pdf';
-import ImportantDocs from '../../assets/files/Application list of documents.pdf';
-import Trustees from '../../assets/files/2019 HENDRICKS COUNTY TRUSTEES.docx';
-import Blessings from '../../assets/files/Locations for Brownsburg Blessing Boxes.docx';
+import ImportantDocs from '../../assets/files/Applicationlistofdocuments.pdf';
+import Trustees from '../../assets/files/2019HENDRICKSCOUNTYTRUSTEES.docx';
+import Blessings from '../../assets/files/LocationsforBrownsburgBlessingBoxes.docx';
 
 export default class Assistance extends React.Component {
 
     render() {
         return (
-            <div className='assistance secondaryContainer'>
+            <div className='assistance secondaryContainer'> 
                 <div className='assistInfo'>
                     <h2>Township Assistance</h2>
                     <p>Brown Township offers temporary assistance to qualifying individuals, in an attempt to help our citizens get back on their own two feet. Individuals and families who wish to apply can find more instructions on Eligibily and next steps below. Those who do not meet the requirements, the Township will attempt to refer those to other programs and counsels available. Township Assistance is a program that allows those in need of temporary relief the opportunity to apply for assistance for food, clothing, shelter, and other basic necessities. The state requires the completion of an application and thorough investigation, and then assists in the most economical means available.</p>
@@ -38,16 +38,17 @@ export default class Assistance extends React.Component {
                 </div>
                 <div>
                     <div>
+                        <h4>Downloads</h4>
                         <h5>Required Documents</h5>
                         <ul>
-                            <li><a href={TA1Form} alt='Township Assistance TA-1 Form'>Township assistance TA-1 Form</a></li>
+                            <li><a href={TA1Form} download alt='Township Assistance TA-1 Form'>Township assistance TA-1 Form</a></li>
                         </ul>
                         <h5>Other Documents</h5>
                         <ul>
-                            <li><a href={Guidelines} alt="Poverty Guidelines 2019" >Poverty Guidelines 2019</a></li>
-                            <li><a href={ImportantDocs} alt="List Of Important Documents">List Of Important Documents</a></li>
-                            <li><a href={Trustees} alt="Hendricks County Trustee Information 2019">Hendricks County Trustee Infomation 2019</a></li>
-                            <li><a href={Blessings} alt="Blessing Box Location List">Blessing Box Locations in Brownsburg</a></li>
+                            <li><a href={Guidelines} download alt="Poverty Guidelines 2019" >Poverty Guidelines 2019</a></li>
+                            <li><a href={ImportantDocs} download alt="List Of Important Documents">List Of Important Documents</a></li>
+                            <li><a href={Trustees} download alt="Hendricks County Trustee Information 2019">Hendricks County Trustee Infomation 2019</a></li>
+                            <li><a href={Blessings} download alt="Blessing Box Location List">Blessing Box Locations in Brownsburg</a></li>
                         </ul>
                     </div>
                     <div className='trusteeInfo'>
